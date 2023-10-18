82c24ad5e36e5a7037b9469ba9bcf97ea9322e43
Adamb7r <bahrcr7@gmail.com>
2023-10-18 09:46:40 -0700

    fixed builtin file

--- builtin1.c
+++ builtin1.c
@@ -54,3 +54,58 @@ int set_alias(info_t *info, char *str)
 	unset_alias(info, str);
 	return (add_node_end(&(info->alias), str, 0) == NULL);
 }
+
+/**
+ * print_alias - this prints an alias the string in code
+ * @node: aliass node
+ * Return: if success 0, if error 1
+*/
+int print_alias(list_t *node)
+{
+        char *q = NULL, *b = NULL;
+
+        if (node)
+        {
+                q = _strchr(node->str, '=');
+                for (b = node->str; b <= q; b++)
+                _putchar(*b);
+                _putchar('\'');
+                _puts(q + 1);
+                _puts("'\n");
+                return (0);
+        }
+        return (1);
+}
+
+/**
+ * _myalias - mimics the alias the builtin
+ * @info: the structure containing arguments. to main prototype.
+ * Return: 0 always.
+*/
+int _myalias(info_t *info)
+{
+        int e = 0;
+        char *q = NULL;
+        list_t *node = NULL;
+
+        if (info->argc == 1)
+        {
+                node = info->alias;
+                while (node)
+                {
+                        print_alias(node);
+                        node = node->next;
+                }
+                return (0);
+        }
+        for (e = 1; info->argv[e]; e++)
+        {
+                q = _strchr(info->argv[e], '=');
+                if (q)
+                        set_alias(info, info->argv[e]);
+                else
+                        print_alias(node_starts_with(info->alias, info->argv[e], '='));
+        }
+
+        return (0);
+}
